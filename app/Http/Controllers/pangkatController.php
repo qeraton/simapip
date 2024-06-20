52d07ad54f08e1be197c4c271d377d53f60c510d
caamanca1 <muhammadrieskyfabiansyah@smkwikrama.sch.id>
2024-06-20 14:39:03 +0700

    MajorFixAtModulPangkat

--- app/Http/Controllers/pangkatController.php
+++ app/Http/Controllers/pangkatController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Redirect;
 use Hash;
 use Session;
@@ -57,38 +59,47 @@ class pangkatController extends Controller
 
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'kode' => 'required|numeric',
-            'nama' => 'required|string|max:225',
-            // 'golongan_kode' => 'required|string|max:10',
-            'golongan_kode_kiri' => 'required|string|max:10',
-            'golongan_kode_kanan' => 'required|string|max:10',
-            'golongan' => 'required|numeric',
-            'urutan' => 'required|numeric',
-
-        ]);
-
-        // Gabungkan kedua bagian menjadi satu kode
-        $golongan_kode = $request->golongan_kode_kiri . '/' . $request->golongan_kode_kanan;
-
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        }
-
         try {
-            
+            $validator = Validator::make($request->all(), [
+                'kode' => 'required|numeric|unique:ref_pangkat,kode',
+                'nama' => 'required|string|max:225',
+                'golongan_kode_kiri' => 'required|string|max:10',
+                'golongan_kode_kanan' => 'required|string|max:10',
+                'golongan' => 'required|numeric',
+                'urutan' => 'required|numeric',
+            ]);
+
+            // Gabungkan kedua bagian menjadi satu kode
+            $golongan_kode_kiri = strtoupper($request->golongan_kode_kiri);
+            $golongan_kode_kanan = strtolower($request->golongan_kode_kanan);
+
+            $golongan_kode = $golongan_kode_kiri . '/' . $golongan_kode_kanan;
+
+            if ($validator->fails()) {
+                throw new ValidationException($validator);
+            }
+
             pangkat::create([
                 'kode' => $request->kode,
                 'nama' => $request->nama,
                 'golongan_kode' => $golongan_kode,
-                'golongan_kode_kiri' => $request->golongan_kode_kiri,
-                'golongan_kode_kanan' => $request->golongan_kode_kanan,
+                'golongan_kode_kiri' => $golongan_kode_kiri,
+                'golongan_kode_kanan' => $golongan_kode_kanan,
                 'golongan' => $request->golongan,
                 'urutan' => $request->urutan,
             ]);
 
-
             return Redirect::to('/pangkat')->with('success', 'Berhasil Menambah data!');
+        } catch (ValidationException $e) {
+            // Periksa apakah kesalahan berasal dari field 'kode'
+            if ($e->validator->errors()->has('kode')) {
+                return Redirect::back()
+                    ->withErrors(['kode' => 'Kode Pangkat Sudah Terdaftar DiDatabase, Silahkan Inputkan Kode Lain!'])
+                    ->withInput();
+            }
+
+            // Tangani validasi lainnya jika ada
+            return Redirect::back()->withErrors($e->validator)->withInput();
         } catch (\Exception $e) {
             dd($e->getMessage()); // Menampilkan pesan error pada pengecualian
             return redirect()->back()->with('error', 'Gagal Menambahkan Data: ' . $e->getMessage());
@@ -112,38 +123,59 @@ class pangkatController extends Controller
 
     public function update(Request $request, pangkat $pangkatController, $id)
     {
-        $request->validate([
-            'kode' => 'numeric',
-            'nama' => 'string|max:225',
-            'golongan_kode_kiri' => 'max:10',
-            'golongan_kode_kanan' => 'max:10',
-            'golongan' => 'numeric',
-            'urutan' => 'numeric',
-        ]);
+        try {
+            $validator = Validator::make($request->all(), [
+                'kode' => 'numeric',
+                'nama' => 'string|max:225',
+                'golongan_kode_kiri' => 'max:10',
+                'golongan_kode_kanan' => 'max:10',
+                'golongan' => 'numeric',
+                'urutan' => 'numeric',
+            ]);
 
-        // Ambil nilai asli golongan_kode dari database
-        $pangkat = pangkat::findOrFail($id);
-        $golongan_kode_asli = $pangkat->golongan_kode;
+            // Ambil nilai asli golongan_kode dari database
+            $pangkat = pangkat::findOrFail($id);
+            $golongan_kode_asli = $pangkat->golongan_kode;
 
-        // Gabungkan kedua bagian golongan_kode_kiri dan golongan_kode_kanan menjadi satu
-        $golongan_kode = $request->golongan_kode_kiri . '/' . $request->golongan_kode_kanan;
+            // Gabungkan kedua bagian golongan_kode_kiri dan golongan_kode_kanan menjadi satu
+            $golongan_kode = strtoupper($request->golongan_kode_kiri) . '/' . strtolower($request->golongan_kode_kanan); // Mengubah golongan_kode_kanan menjadi huruf kecil
 
-        // Periksa apakah kedua input golongan_kode_kiri dan golongan_kode_kanan kosong
-        if (empty($request->golongan_kode_kiri) || empty($request->golongan_kode_kanan)) {
-            // Jika salah satu atau kedua input kosong, gunakan nilai asli dari golongan_kode
-            $golongan_kode = $golongan_kode_asli;
+            // Periksa apakah kedua input golongan_kode_kiri dan golongan_kode_kanan kosong
+            if (empty($request->golongan_kode_kiri) || empty($request->golongan_kode_kanan)) {
+                // Jika salah satu atau kedua input kosong, gunakan nilai asli dari golongan_kode
+                $golongan_kode = $golongan_kode_asli;
+            }
+
+            if ($validator->fails()) {
+                throw new ValidationException($validator);
+            }
+
+            pangkat::where('id', $id)->update([
+                'kode' => $request->kode,
+                'nama' => $request->nama,
+                'golongan_kode' => $golongan_kode,
+                'golongan' => $request->golongan,
+                'urutan' => $request->urutan,
+            ]);
+
+            return Redirect::to('/pangkat')->with('success', 'Berhasil mengubah data!');
+        } catch (ValidationException $e) {
+            // Periksa apakah kesalahan berasal dari field 'kode'
+            if ($e->validator->errors()->has('kode')) {
+                return Redirect::back()
+                    ->withErrors(['kode' => 'Kode sudah ada didatabase, gunakan kode lain'])
+                    ->withInput();
+            }
+
+            // Tangani validasi lainnya jika ada
+            return Redirect::back()->withErrors($e->validator)->withInput();
+        } catch (\Exception $e) {
+            dd($e->getMessage()); // Menampilkan pesan error pada pengecualian
+            return redirect()->back()->with('error', 'Gagal mengubah data: ' . $e->getMessage());
         }
-
-        pangkat::where('id', $id)->update([
-            'kode' => $request->kode,
-            'nama' => $request->nama,
-            'golongan_kode' => $golongan_kode,
-            'golongan' => $request->golongan,
-            'urutan' => $request->urutan,
-        ]);
-
-        return Redirect::to('/pangkat')->with('success', 'Berhasil mengubah data!');
     }
+
+
 
     public function destroy(pangkat $pangkatController, $id)
     {
--- resources/views/Pangkat/create.blade.php
+++ resources/views/Pangkat/create.blade.php
@@ -1,81 +1,130 @@
-@include('Layout.Header')
-			<!-- NFTmax Dashboard -->
-			<section class="nftmax-adashboard nftmax-show">
-				<div class="container">
-					<div class="row">	
-						<div class="col-lg-9 col-12 nftmax-main__column">
-							<div class="nftmax-body">
-								<!-- Dashboard Inner -->
-								<div class="nftmax-dsinner">
-									<!-- All Notification Heading -->
-									<div class="nftmax-inner__heading">
-										<h2 class="nftmax-inner__page-title">Tambah Pangkat Baru</h2>
-									</div>
-									<!-- End All Notification Heading -->
-									
-									<div class="nftmax__item">
-										
-										<div class="nftmax__item-heading">
-											<h2 class="nftmax__item-title nftmax__item-title--psingle">Silahkan Inputkan Informasi Terkait Pangkat</h2>
-											<p class="nftmax__item-text nftmax__item-text--single">Mohon Input Data Yang Valid!</p>
-										</div>
-										<form class="form" method="POST" action="{{ url('pangkat/store') }}">
-                      @csrf
-											<div class="row">
-												<div class="col-12">
-													<div class="nftmax__item-box">
-                                  <div class="nftmax__item-form--group">
-																		<label class="nftmax__item-label">Kode </label>
-																		<input class="nftmax__item-input" type="number" name="kode" id="kode" placeholder="Input Kode" required="required">
-																	</div>
-																	
-																	<div class="nftmax__item-form--group">
-																		<label class="nftmax__item-label">Nama Pangkat </label>
-																		<input class="nftmax__item-input" type="text" name="nama" id="nama" placeholder="Input Nama Pangkat" required="required">
-																	</div>
-																	
-																	{{-- <div class="nftmax__item-form--group">
-																		<label class="nftmax__item-label">Kode Golongan </label>
-																		<input class="nftmax__item-input" type="text" name="golongan_kode" id="golongan_kode" placeholder="Input Kode Golongan" required="required">
-																	</div> --}}
+@include('Layout.header')
+<!-- NFTmax Dashboard -->
+<section class="nftmax-adashboard nftmax-show">
+    <div class="container">
+        <div class="row">	
+            <div class="col-lg-9 col-12 nftmax-main__column">
+                <div class="nftmax-body">
+                    <!-- Dashboard Inner -->
+                    <div class="nftmax-dsinner">
+                        <!-- All Notification Heading -->
+                        <div class="nftmax-inner__heading">
+                            <h2 class="nftmax-inner__page-title">Tambah Data Pangkat</h2>
+                        </div>
+                        <!-- End All Notification Heading -->
+                    
+                        <div class="nftmax-personals">
 
-                                  <div class="nftmax__item-form--group">
-                                    <label class="nftmax__item-label">Kode Golongan </label>
-                                    <div class="input-group input-group-sm w-50 m-4">
-                                      <input class="form-control" type="text" name="golongan_kode_kiri" id="golongan_kode_kiri" placeholder="Input, I, II, III, IV" required="required">
-                                      <div class="input-group-prepend input-group-append">
-                                        <div class="input-group-text">/</div>
-                                      </div>
-                                      <input class="form-control" type="text" name="golongan_kode_kanan" id="golongan_kode_kanan" placeholder="Input, a, b, c, d" required="required"></input>
+                            <div class="button-form-back">
+                                <button id="btnCancel" class="btnCancel" data-toggle="modal" data-target="#confirmationModal">
+                                    <i class="fas fa-chevron-left"></i> Kembali
+                                </button>
+                            </div>
+
+                            <!-- Modal Confirmation -->
+                            <div id="confirmationModal" class="modal fade">
+                                <div class="modal-dialog modal-confirm modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header flex-column">
+                                            <div class="icon-box">
+                                                <i class="material-icons">&#xE5CD;</i>
+                                            </div>						
+                                            <h4 class="modal-title w-100">Apakah Anda Yakin?</h4>	
+                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Yakin ingin kembali ke halaman?, Progress Anda Tidak Akan Tersimpan.</p>
+                                        </div>
+                                        <div class="modal-footer justify-content-center">
+                                            <button type="button" data-dismiss="modal" aria-label="btn-cancel" class="btn btn-secondary btn-cancel">Tidak</button>
+                                            <button onclick="window.location.href = '<?php echo url('/pangkat'); ?>'" class="btn btn-danger" type="submit">Ya</button>
+                                        </div>
                                     </div>
-																	</div>
+                                </div>
+                            </div>    
 
-                                  <div class="nftmax__item-form--group">
-																		<label class="nftmax__item-label">Golongan </label>
-																		<input class="nftmax__item-input" type="number" name="golongan" id="golongan" placeholder="Input golongan" required="required">
-																	</div>
+                            <h2 class="nftmax__item-title nftmax__item-title--psingle">Silahkan Input Data Pangkat Sesuai Dengan Kriteria!</h2>
+                            <p class="nftmax__item-text nftmax__item-text--single mb-4">Mohon Input Data Yang Valid!</p>
+                            <hr>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-2 col-12 nftmax-personals__list">
+                                    <div class="nftmax-psidebar">
+                                            <!-- Features Tab List -->
+                                            <div class="list-group nftmax-psidebar__list" id="list-tab" role="tablist">
+                                                <a class="list-group-item active" data-bs-toggle="list" href="#id1" role="tab">
+                                                    <span class="nftmax-psidebar__icon">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
+                                                            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                                                            <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0"/>
+                                                        </svg>
+                                                    </span>
+                                                    <span class="nftmax-psidebar__title">Tambah Pangkat </span>
+                                                </a>
+                                            </div>
+                                    </div>
+                                </div>
+                                    
+                                <div class="col-lg-9 col-md-10 col-12 nftmax-personals__content">
+                                <div class="nftmax-ptabs">
+                                    <form class="form" method="POST" action="{{ url('pangkat/store') }}">
+                                        @csrf
+                                            <div class="nftmax-ptabs__inner">
+                                                <div class="tab-content" id="nav-tabContent">
+                                                <!--  Features Single Tab -->
+                                                <div class="tab-pane fade show active" id="id1" role="tabpanel">
+                                                    <div class="nftmax__item-box">
+																											<div class="nftmax__item-form--group">
+																												<label class="nftmax__item-label">Kode </label>
+																												<input class="nftmax__item-input" type="number" name="kode" id="kode" placeholder="Input Kode" required="required">
+																											</div>
+																											
+																											<div class="nftmax__item-form--group">
+																												<label class="nftmax__item-label">Nama Pangkat </label>
+																												<input class="nftmax__item-input" type="text" name="nama" id="nama" placeholder="Input Nama Pangkat" required="required">
+																											</div>
+																											
+																											<div class="nftmax__item-form--group">
+																													<label class="nftmax__item-label">Kode Golongan </label>
+																													<div class="input-group input-group-sm">
+																															<input type="text" class="form-control" name="golongan_kode_kiri" placeholder="Input, I, II, III, IV" style="height: 40px;" required>
+																															<div class="input-group-append">
+																																	<span class="input-group-text" style="height: 40px;">/</span>
+																															</div>
+																															<input type="text" class="form-control" name="golongan_kode_kanan" placeholder="Input, a, b, c, d" style="height: 40px;" required>
+																													</div>
+																											</div>
+																											
+																											<div class="nftmax__item-form--group">
+																												<label class="nftmax__item-label">Golongan </label>
+																												<input class="nftmax__item-input" type="number" name="golongan" id="golongan" placeholder="Input golongan" required="required">
+																											</div>
 
-                                  <div class="nftmax__item-form--group">
-																		<label class="nftmax__item-label">Urutan </label>
-																		<input class="nftmax__item-input" type="number" name="urutan" id="urutan" placeholder="Input urutan" required="required">
-																	</div>
+																											<div class="nftmax__item-form--group">
+																												<label class="nftmax__item-label">Urutan </label>
+																												<input class="nftmax__item-input" type="number" name="urutan" id="urutan" placeholder="Input urutan" required="required">
+																											</div>
 
-															</div>
-														</div>
-													</div>
-													<div class="nftmax__item-button--group">
-                            <a class="nftmax__item-button--single nftmax__item-button--cancel" href="{{ url('/pangkat') }}" type="submit">Batal</a>
-														<button class="nftmax__item-button--single nftmax-btn nftmax-btn__bordered bg radius nftmax-item__btn" type="submit">Simpan</button>
-														
-													</div>
-												</div>
-											</div>
-										</form>	
-									</div>
-									
-								</div>
-								<!-- End Dashboard Inner -->
-							</div>
-						</div>
+																									</div>
+																								</div>
+																							</div>
+                                                </div>
+                                            </div>
+                                </div>
+                            </div>
+														<div class="nftmax__item-button--group">
+                                            <button
+                                                class="nftmax__item-button--single nftmax-btn nftmax-btn__bordered bg radius nftmax-item__btn"
+                                                type="submit">Tambah</button>
+                                        </div>
+                                    </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Dashboard Inner -->
+    </div>
+</div>
+
 {{-- @include('Layout.RightSidebar') --}}
 @include('Layout.Footer')
